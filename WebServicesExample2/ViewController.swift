813b2e60fe0df5f2f1f983febb0289be0adfd4cc
VincentDay <vincent.day@gmail.com>
2015-12-11 09:43:48 -0500

    swift son not working

--- WebServicesExample2/ViewController.swift
+++ WebServicesExample2/ViewController.swift
@@ -7,12 +7,43 @@
 //
 
 import UIKit
+import SwiftyJSON
+
+
 
 class ViewController: UIViewController {
+    
 
+    @IBOutlet weak var forecastLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let json = JSON(responseObject)
+        if let forecast = json["list"][0]["weather"][0]["description"].string {
+            self.forecastLabel.text = forecast
+        }
+        
+        let manager = AFHTTPSessionManager()
+        manager.GET("http://api.openweathermap.org/data/2.5/forecast/daily?q=London&mode=json&units=metric&cnt=1&appid=62371582211be3320c39fc6f1835e84b", parameters: nil,
+            progress: { (progress: NSProgress) -> Void in
+            
+            }, success: { (task: NSURLSessionDataTask, responseObject: AnyObject?) -> Void in
+                print("JSON: " + responseObject!.description)
+                if let listOfDays = responseObject!["list"] as? NSArray {
+                    if let tomorrow = listOfDays[0] as? NSDictionary {
+                        if let tomorrowsWeather = tomorrow["weather"] as? NSArray {
+                            if let firstWeatherOfDay = tomorrowsWeather[0] as? NSDictionary {
+                                if let forecast = firstWeatherOfDay["description"] as? String {
+                                    self.forecastLabel.text = forecast
+                                }
+                            }
+                        }
+                    }
+                }
+            }) { (task: NSURLSessionDataTask?, error: NSError) -> Void in
+                print("Error: " + error.localizedDescription)
+        }
     }
 
     override func didReceiveMemoryWarning() {
---
+++ bridge.h
@@ -0,0 +1,15 @@
+//
+//  bridge.h
+//  WebServicesExample2
+//
+//  Created by Vince Day on 12/9/15.
+//  Copyright © 2015 Vince Day. All rights reserved.
+//
+
+#ifndef bridge_h
+#define bridge_h
+#import "AFNetworking/AFNetworking.h"
+
+
+
+#endif /* bridge_h */
